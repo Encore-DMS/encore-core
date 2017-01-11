aa29e7eb729296d67cf4c3855843b77fc760de76
Mark Cafaro <markcafaro@D-128-95-10-211.dhcp4.washington.edu>
2017-01-10 16:23:45 -0800

    Fixed gradle for junit5

--- src/test/java/io/github/encore_dms/domain/jpa/JPADataContextTest.java
+++ src/test/java/io/github/encore_dms/domain/jpa/JPADataContextTest.java
@@ -10,9 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JPADataContextTest extends JPATest {
 
